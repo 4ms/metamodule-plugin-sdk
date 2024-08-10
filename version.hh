b8858d30ac867b1128d6deb5283a0cde2acca159
Dan Green <danngreen1@gmail.com>
2024-08-09 22:05:56 -0700

    MetaModule::Version::is_compatible

--- version.hh
+++ version.hh
@@ -1,12 +1,19 @@
 #pragma once
 #include <cstdint>
 
-namespace MetaModule {
+namespace MetaModule
+{
 
 struct Version {
-  uint8_t major;
-  uint8_t minor;
-  uint8_t revision;
+	uint8_t major;
+	uint8_t minor;
+	uint8_t revision;
+
+	bool is_compatible(Version other) {
+		return major == other.major && minor == other.minor;
+	}
 };
+
+Version sdk_version();
 
 } // namespace MetaModule
