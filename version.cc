98f887f481028ca67e66f5ecd7317cd344353ea6
Dan Green <danngreen1@gmail.com>
2024-08-09 21:40:04 -0700

    Use Version struct

--- version.cc
+++ version.cc
@@ -1,13 +1,13 @@
-#include <cstdint>
+#include "version.hh"
 
-__attribute__((__visibility__("default"))) uint32_t sdk_version() {
-  constexpr uint32_t MAJOR = 0;
-  constexpr uint32_t MINOR = 15;
-  constexpr uint32_t REVISION = 0;
+namespace MetaModule {
 
-  constexpr uint32_t MAJOR_M = 10000;
-  constexpr uint32_t MINOR_M = 100;
-  constexpr uint32_t REVISION_M = 1;
-
-  return (MAJOR * MAJOR_M) + (MINOR * MINOR_M) + (REVISION * REVISION_M);
+__attribute__((__visibility__("default"))) Version sdk_version() {
+  return Version{
+      .major = 0,
+      .minor = 15,
+      .revision = 0,
+  };
 }
+
+} // namespace MetaModule
---
+++ version.hh
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdint>
+
+namespace MetaModule {
+
+struct Version {
+  uint8_t major;
+  uint8_t minor;
+  uint8_t revision;
+};
+
+} // namespace MetaModule
