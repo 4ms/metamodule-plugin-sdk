f0a0c9c2f4f1194dc1bc6141b47d2b235cce38c2
Dan Green <danngreen1@gmail.com>
2024-09-18 13:24:55 -0700

    Take any type for Version::can_host_version

--- version.hh
+++ version.hh
@@ -9,7 +9,7 @@ struct Version {
 	uint8_t minor;
 	uint8_t revision;
 
-	bool can_host_version(Version other) {
+	bool can_host_version(auto other) {
 		return major == other.major && minor >= other.minor;
 	}
 };
