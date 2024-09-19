7b14680aea1aa33e955f91142454a14352599f50
Dan Green <danngreen1@gmail.com>
2024-09-19 09:21:51 -0700

    Use cpputil Version tools

--- version.hh
+++ version.hh
@@ -1,22 +1,11 @@
 #pragma once
+#include "util/version_tools.hh"
 #include <cstdint>
 
 namespace MetaModule
 {
 
-struct Version {
-	uint8_t major;
-	uint8_t minor;
-	uint8_t revision;
-
-	bool can_host_version(auto other) {
-		return major == other.major && minor >= other.minor;
-	}
-	bool is_later(auto other) {
-		return (major > other.major) || (major == other.major && minor > other.minor) ||
-			   (major == other.major && minor == other.minor && revision > other.revision);
-	}
-};
+using Version = VersionUtil::Version;
 
 Version sdk_version();
 
