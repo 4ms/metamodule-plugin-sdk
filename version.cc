efc1f35e7baf380681e56ddbb780bdb49649b723
Dan Green <danngreen1@gmail.com>
2024-12-05 16:18:21 -0800

    SDK Version => 1.6.0

--- version.cc
+++ version.cc
@@ -1,10 +1,9 @@
 #include "version.hh"
 
-namespace MetaModule
-{
+namespace MetaModule {
 
 __attribute__((__visibility__("default"))) Version sdk_version() {
-	return Version{1, 4, 0};
+  return Version{1, 6, 0};
 }
 
 } // namespace MetaModule
