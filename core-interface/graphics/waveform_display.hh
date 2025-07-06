9d2a6d502805adf672cb14d4b37bcfdd1dc416f2
Dan Green <danngreen1@gmail.com>
2025-07-06 14:27:14 -0700

    StreamingWaveformDisplay implmentation moved to firmware

--- core-interface/graphics/waveform_display.hh
+++ core-interface/graphics/waveform_display.hh
@@ -1,176 +1,93 @@
 #pragma once
 #include "CoreModules/CoreProcessor.hh"
-#include "CoreModules/elements/units.hh"
-#include "thorvg/thorvg/inc/thorvg.h"
-#include <algorithm>
 #include <atomic>
-#include <cmath>
+#include <cstdint>
+#include <memory>
+#include <vector>
 
 namespace MetaModule
 {
 
+// StreamingWaveformDisplay
+// class to display a streaming waveform, when we only know
+// the value of the waveform one sample at a time
+// (as opposed to displaying a waveform which we know all the values of)
+// The waveform is drawn with the oldest sample at the far left
+// and newer samples progressing to the right
+//
+// Usage:
+//
+//   struct MyModule : CoreProcessor {
+//     	StreamingWaveformDisplay waveform;
+//
+//     	MyModule() {
+//     		waveform.set_wave_color(0x33, 0xFF, 0xBB); //teal
+//     	}
+//
+//     	void update() override {
+//     		float sample = get_next_sample();
+//     		waveform.draw_sample(sample);
+//     	}
+//
+//     	void show_graphic_display(int display_id, std::span<uint32_t> buf, unsigned width, lv_obj_t *canvas) override {
+//     		waveform.show_graphic_display(buf, width, canvas);
+//     	}
+//
+//     	bool draw_graphic_display(int display_id) override {
+//     		return waveform.draw_graphic_display();
+//     	}
+//
+//     	void hide_graphic_display(int display_id) override {
+//     		waveform.hide_graphic_display();
+//     	}
+//   };
+
 class StreamingWaveformDisplay {
 public:
-	// TSP: (24mm, 23.4mm)
-	StreamingWaveformDisplay(float display_width_mm, float display_height_mm)
-		: display_width{mm_to_px(display_width_mm, 240)}
-		, display_height{mm_to_px(display_height_mm, 240)}
-		, wave_height{(display_height - bar_height) / 2.f} {
+	// Construct the waveform with the dimensions of the graphic display in mm.
+	// This must fit within the bounds of a graphic display of your module.
+	StreamingWaveformDisplay(float display_width_mm, float display_height_mm);
+	~StreamingWaveformDisplay();
 
-		samples.resize(320);
-	}
+	// Add a sample to the display
+	// Depending on the x_zoom, it might not be drawn until enough new
+	// samples have been received.
+	void draw_sample(float sample);
 
-	void set_cursor_position(float pos) {
-		cursor_pos = pos;
-	}
+	// Reset the waveform to 0's and start displaying from the left edge
+	void sync();
 
-	void draw_sample(float sample) {
-		sample = std::clamp(sample, -1.f, 1.f);
+	// Sets the "zoom" along the x-axis, by setting the number of samples
+	// represented by one x coordinate of the pixel grid.
+	// Typical values would be from 1 to 500.
+	// For each x coordinate of the graphic display,
+	// the minimum and maximum sample values of the samples represented by that
+	// x coordinate will be drawn.
+	void set_x_zoom(float zoom);
 
-		oversample_min = std::min(oversample_min, sample);
-		oversample_max = std::max(oversample_max, sample);
+	// Sets the color of the waveform
+	void set_wave_color(uint8_t r, uint8_t g, uint8_t b);
 
-		x_zoom_ctr += x_zoom;
-		if (x_zoom_ctr > 1.f) {
-			x_zoom_ctr = 0;
+	// Sets the color of the bar at the bottom that the cursor travels along
+	void set_bar_color(uint8_t r, uint8_t g, uint8_t b);
 
-			{
-				auto t = newest_sample.load();
+	// Sets the width of the cursor in unscaled pixels
+	void set_cursor_width(unsigned width);
 
-				if (++t >= (int)samples.size())
-					t = 0;
+	// Sets the position of the cursor along the bar
+	// 0 => At the left end
+	// 1 => At the right end
+	void set_cursor_position(float pos);
 
-				samples[t] = {oversample_min, oversample_max};
-
-				newest_sample.store(t);
-			}
-
-			oversample_min = 1.f;
-			oversample_max = -1.f;
-		}
-	}
-
-	void sync() {
-		std::ranges::fill(samples, std::pair<float, float>{0, 0});
-		newest_sample = 0;
-	}
-
-	void set_x_zoom(float zoom) {
-		x_zoom = zoom;
-	}
-
-	void set_wave_color(uint8_t r, uint8_t g, uint8_t b) {
-		wave_r = r;
-		wave_g = g;
-		wave_b = b;
-	}
-	void set_bar_color(uint8_t r, uint8_t g, uint8_t b) {
-		bar_r = r;
-		bar_g = g;
-		bar_b = b;
-	}
-
-	void set_cursor_width(unsigned width) {
-		cursor_width = width;
-	}
-
-	// Functions below here run in the GUI thread and may get interrupted by the functions above
-
-	void show_graphic_display(std::span<uint32_t> pix_buffer, unsigned width, lv_obj_t *lvgl_canvas) {
-		canvas = tvg::SwCanvas::gen();
-		auto scene = tvg::Scene::gen();
-
-		auto scaled_height = pix_buffer.size() / width;
-		canvas->target(pix_buffer.data(), width, width, scaled_height, tvg::ColorSpace::ARGB8888);
-		scaling = float(width) / display_width;
-		scene->scale(scaling);
-
-		// Bar to represent entire sample
-		auto bar = tvg::Shape::gen();
-		bar->appendRect(0, display_height - bar_height / scaling, display_width, bar_height / scaling);
-		bar->fill(bar_r, bar_g, bar_b, 0xFF);
-		scene->push(bar);
-
-		// Trolley to indicate position
-		bar_cursor = tvg::Shape::gen();
-		bar_cursor->appendRect(0, display_height - bar_height / scaling, cursor_width, bar_height / scaling);
-		bar_cursor->fill(0xFF, 0xFF, 0xFF, 0xFF);
-		scene->push(bar_cursor);
-
-		// Black background
-		auto wave_bg = tvg::Shape::gen();
-		wave_bg->appendRect(0, 0, display_width, display_height - bar_height);
-		wave_bg->fill(0x00, 0x00, 0x00, 0xFF);
-		scene->push(wave_bg);
-
-		// Waveform
-		wave = tvg::Shape::gen();
-		wave->moveTo(display_width, wave_height);
-		wave->lineTo(0, wave_height);
-		wave->strokeFill(wave_r, wave_g, wave_b, 0xFF);
-		wave->strokeWidth(1.0f);
-		scene->push(wave);
-
-		canvas->push(scene);
-	}
-
-	bool draw_graphic_display() {
-		if (!canvas)
-			return false;
-
-		bar_cursor->translate(scaling * cursor_pos * (display_width - cursor_width), 0);
-		bar_cursor->scale(scaling);
-		canvas->update(bar_cursor);
-
-		wave->reset();
-
-		//start with the oldest sample
-		int start = newest_sample.load() + 1;
-
-		// Draw top contour left->right
-		int i = start;
-		for (auto x = 0u; x < samples.size(); x++) {
-			float x_pos = (float)x * display_width / (float)samples.size();
-			float y_pos = samples[i].first * wave_height + wave_height;
-			if (x == 0)
-				wave->moveTo(x_pos, y_pos);
-			else
-				wave->lineTo(x_pos, y_pos);
-			i = (i + 1) % samples.size();
-		}
-
-		// Continue shape, drawing bottom contour right->left
-		for (int x = samples.size() - 1; x >= 0; x--) {
-			if (--i < 0)
-				i = samples.size() - 1;
-			float x_pos = (float)x * display_width / (float)samples.size();
-			float y_pos = samples[i].second * wave_height + wave_height;
-			wave->lineTo(x_pos, y_pos);
-		}
-
-		wave->strokeWidth(1.0f / scaling);
-		wave->strokeFill(wave_r, wave_g, wave_b, 0xFF);
-		wave->fill(wave_r, wave_g, wave_b, 0x7F);
-		wave->scale(scaling);
-
-		canvas->update(wave);
-		canvas->draw();
-		canvas->sync();
-
-		return true;
-	}
-
-	void hide_graphic_display() {
-		if (canvas) {
-			delete canvas;
-			canvas = nullptr;
-		}
-	}
+	// These three fuctions should be called from the similarly named CoreProcessor::*_graphic_display
+	// functions in your module code. See example above
+	void show_graphic_display(std::span<uint32_t> pix_buffer, unsigned width, lv_obj_t *lvgl_canvas);
+	bool draw_graphic_display();
+	void hide_graphic_display();
 
 private:
-	tvg::SwCanvas *canvas = nullptr;
-	tvg::Shape *bar_cursor = nullptr;
-	tvg::Shape *wave = nullptr;
+	struct Internal;
+	std::unique_ptr<Internal> internal;
 
 	std::vector<std::pair<float, float>> samples;
 	std::atomic<int> newest_sample = 0;
